391457766d4547c32b6e79b56a569e62270c8610
Yuya Inagaki <yuya1756@live.jp>
2019-10-06 23:25:24 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', (e) => {
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = function() {
+        img.removeAttribute('data-src');
+      };
+    });
+});
